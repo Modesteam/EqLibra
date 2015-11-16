fcedab6ebf6311c93097d96f6fae475a5bb4c7cc
Fillipe Feitosa <fillfeitosa@gmail.com>
2015-11-16 14:20:34 -0200

    Fix nas páginas de Usuário e nas rotas padrão

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :wallets
   devise_for :users
-  root to: 'pages#index'
+  root to: 'pages#home'
+  get '/home', to: 'pages#home', as: :home
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
