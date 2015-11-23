498a70e71edb991ca0bdfb0b67ee158f91d8dc6f
Fillipe Feitosa <fillfeitosa@gmail.com>
2015-11-23 21:00:27 -0200

    Us03 Completa. Conectando com Facebook

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122181430) do
+ActiveRecord::Schema.define(version: 20151123225551) do
 
   create_table "direct_treasuries", force: :cascade do |t|
     t.string   "title"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 20151122181430) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
