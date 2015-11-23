4c46b7dd19a3c53fb42658c223c5e482a782c455
Matheus Ferraz <matheus_ccmf@yahoo.com.br>
2015-11-23 16:22:13 -0200

    Adição dos testes de criação do Inv.
    Signed-off-by: Augusto Modesto <augusto.modesto@live.com>
    Signed-off-by: Matheus Ferraz <matheus_ccmf@yahoo.com.br>

--- features/investment_fund/create.feature
+++ features/investment_fund/create.feature
@@ -2,20 +2,19 @@ Feature: Investment Fund Creation
   In order to register my investment fund
   As an user
   I should be able to create an investment fund
-
-  Scenario: Should not create investment fund without login
-    Given I am on the investment_funds
-    Then I should not see "New Investment fund"
-    And I should see "You need to sign in or sign up before continuing."
     
   Scenario: Investment fund creation
     Given I am an user
     And I am signed in
-    And I have a Wallet
-    And I am on the investment_funds
-    When I click on New Investment fund link
+    And I am at the homepage
+    When I choose the wallets option
+    Then I should see "Listing Wallets"
+    When I choose investment fund option
+    Then I should see "Listing Investment Funds"
+    When I choose the new investment fund option
+    Then I should see "New Investment Fund"
     And I fill in "investment_fund_name" with "Meu Fundo BB de Investimento"
-	And I select date "2015 June 29" as the "investment_fund_buyDate"
+	  And I select date "2015 June 29" as the "investment_fund_buyDate"
     And I fill in "investment_fund_wallet_id" with "1"
     When I press the "Create Investment fund" button
     Then I should see "Nome: Meu Fundo BB de Investimento"
--- features/step_definitions/investment_funds_steps.rb
+++ features/step_definitions/investment_funds_steps.rb
@@ -1,43 +1,17 @@
-Given(/^I am on the investment_funds$/) do
-  visit investment_funds_path
+When(/^I choose investment fund option$/) do
+   click('Fundos de Inv.')
 end
 
-Given(/^there's a sample low duration investment fund$/) do
-  @investfund_lowDuration = FactoryGirl.create(:investfund_lowDuration)
+When(/^I choose the new investment fund option$/) do
+   click_link "New Investment Fund"
 end
 
-Given(/^there's a sample medium duration investment fund$/) do
-  @investfund_mediumDuration = FactoryGirl.create(:investfund_mediumDuration)
+When(/^I choose the wallets option$/) do
+   click_link "Carteiras"
 end
 
-Given(/^there's a sample regular duration investment fund$/) do
-  @investfund_regularDuration = FactoryGirl.create(:investfund_regularDuration)
-end
-
-When(/^I click on New Investment fund link$/) do
-  visit new_investment_fund_path
-end
-
-When(/^I click the Edit investment fund link$/) do
-  visit edit_investment_fund_path(@investment_fund)
-end
-
-Given(/^there's a sample no duration investment fund$/) do
-  @investfund_new = FactoryGirl.create(:investfund_new)
-end
-
-Given(/^there's a sample investment fund$/) do
-  @investment_fund = FactoryGirl.create(:investment_fund)
-end
-
-When(/^I click on Destroy investment_fund link$/) do
-  InvestmentFund.destroy(@investment_fund.id)
-end
-
-Then(/^the investment_fund should not be there$/) do
-  expect { InvestmentFund.find(@investment_fund.id) }.to raise_error
-end
-
-Then(/^I should be in the Edit Investment_fund page$/) do
-  expect(page).to have_content("Editing Investment Fund")
+And /^(?:|I )select date "([^ ]*) ([^ ]*) ([^ ]*)" as the "([^"]*)"$/ do |year, month, day, field|
+  select(year,   :from => "#{field}_1i")
+  select(month,  :from => "#{field}_2i")
+  select(day,    :from => "#{field}_3i")
 end
